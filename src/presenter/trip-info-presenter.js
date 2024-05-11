a04af7bddb5528cf8f11c4b6acda36a19c10b434
Anton Rychkov <antrychkov@gmail.com>
2024-05-11 15:35:10 +0500

    1.3 Добавляет модуль точки

--- src/presenter/trip-info-presenter.js
+++ src/presenter/trip-info-presenter.js
@@ -1,6 +1,9 @@
 import EventsListView from '../view/events-list-view.js';
 import SortView from '../view/sort-view.js';
 import AddPointFormView from '../view/add-point-form-view.js';
+import EditPointFormView from '../view/edit-point-form-view.js';
+import PointView from '../view/point-view.js';
+import { RenderPosition } from '../render.js';
 import {render} from '../render.js';
 
 
@@ -12,8 +15,13 @@ export default class TripInfoPresenter {
   }
 
   init() {
-    render(new SortView(), this.eventsListComponent.getElement());
+    render(new SortView(), this.eventsListContainer);
     render(this.eventsListComponent, this.eventsListContainer);
     render(new AddPointFormView(), this.eventsListComponent.getElement());
+    render(new EditPointFormView(), this.eventsListComponent.getElement(), RenderPosition.AFTERBEGIN);
+
+    for(let i = 0; i < 3; i++) {
+      render(new PointView(), this.eventsListComponent.getElement());
+    }
   }
 }
